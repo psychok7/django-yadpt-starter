942d149fe13c6d68629e74795afcb20f6c3e9046
Nuno Khan <nunok7@gmail.com>
2016-04-29 16:29:51 +0100

    removed full version for now

--- full/compose/django/staging/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Based on http://stackoverflow.com/a/28508138/977622 for cleaning celery .pid
-echo "Removing .pid files"
-rm -f *.pid
-exec "$@"
--- full/users/api/authentication.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.authentication import BaseAuthentication
-
-
-class NoAuthentication(BaseAuthentication):
-    def authenticate(self, request):
-        return None
