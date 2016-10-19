3e1407f1232f5f93da0f1f0c562dd440b5bf737d
Nuno Khan <nunok7@gmail.com>
2016-10-19 19:00:42 +0100

    renamed start script, removed unwanted files and preparing installer

--- minimal/compose/django/staging/gunicorn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python3 manage.py makemigrations --settings={{ project_name }}.settings.staging
-python3 manage.py migrate --settings={{ project_name }}.settings.staging
-python3 manage.py collectstatic --noinput --settings={{ project_name }}.settings.staging
-gunicorn {{ project_name }}.wsgi:application -w 4 -t 120 -e DJANGO_SETTINGS_MODULE={{ project_name }}.settings.staging -b :8004
-
-#python3 manage.py runserver 0.0.0.0:8004 --settings={{ project_name }}.settings.staging
